650dbfcafa35812f8a4a8ca970bcca7377d12f3b
Samweli <smwltwesa6@gmail.com>
2017-02-01 13:26:34 +0300

    modified header for analytics

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -24,7 +24,7 @@ class GAController(BaseController):
     def view(self):
         # get package objects corresponding to popular GA content
         c.top_resources = dbutil.get_top_resources(limit=10)
-        c.top_packages = dbutil.get_top_packages(limit=10)
+        # c.top_packages = dbutil.get_top_packages(limit=10)
         return render('summary.html')
 
 
